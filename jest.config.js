5caf299e2eff2a5751783e4c667df19414fc3c5e
rglov <github@glover.simplelogin.com>
2026-02-22 13:45:58 -0600

    chore: use modern ts-jest transform config style

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,7 @@ module.exports = {
     '^obsidian$': '<rootDir>/tests/__mocks__/obsidian.ts',
   },
   testMatch: ['**/tests/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        module: 'CommonJS',
-      },
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { module: 'CommonJS' } }],
   },
 };
